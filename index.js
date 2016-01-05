6a2181bddc0da68cd3cd4e48b9a1a31fd47d2757
Slimeria <Slimeria37@gmail.com>
2016-01-05 15:39:57 +0800

    修复异步异常的捕获遗漏问题

--- index.js
+++ index.js
@@ -12,8 +12,8 @@ var wechat = require('wechat');
 // var weixinService = require('./server/service/weixinService');
 var constants = require('./server/helpers/constants');
 var authority = require('./server/filter/authority');
-var errorhandler = require('errorhandler');
-var colors = require('colors');
+var domain=require('domain');
+var exception=require('./server/helpers/exception')
 app.use(express.static(__dirname + '/public')); //方便开发，暂时引入
 // App 全局配置
 app.use(cookieParser());
@@ -28,69 +28,17 @@ app.use(bodyParser.urlencoded({
     limit: '2mb',
     extended: true
 }));
-//注入URL
-require('./server/controllers/routes')(app);
-//异常处理
-app.use(errorHandle);
-
+//捕获异步引起的异常
+app.use(function(req, res, next) {
+     exception.nioExceptionHandle(domain,req,res,next);
+});
 //微信开发需要开启
 // weixinService.getAccess_token(); //开启微信获取token定时任务
 // app.use('/weixin', wechat(constants.WeixinConstants.TOKEN, weixinService.payAttentionTo));
+//注入URL
+require('./server/controllers/routes')(app);
+//捕获同步引起的异常
+app.use(exception.bioExceptionHandle);
+
 var appServer = new Server(app);
 appServer.start();
-
-//捕获链式无法捕获的异常
-process.on('uncaughtException', function(err) {
-    const title = String(err);
-    const content = String(err.stack);
-    if (process.env.NODE_ENV == 'production' || process.env.NODE_ENV == 'PRODUCTION') {
-        logger.error('error:' + content);
-    } else {
-        console.info(colors.red('Error info is, "%s" .'), title);
-        console.info(colors.green('Error stack is, "%s" .'), content);
-        require('node-notifier').notify({
-            title: title,
-            message: content
-        })
-    }
-})
-
-//异常处理
-function errorHandle(err, req, res, next) {
-    const stack = err.stack
-    let str;
-    if (stack) {
-        str = String(stack)
-    } else {
-        str = String(err)
-    }
-    //正式生产环境
-    if (err) {
-        if (process.env.NODE_ENV == 'production' || process.env.NODE_ENV == 'PRODUCTION') {
-            if (req.method == "post") {
-                logger.error("method:" + req.method + ',url:' + req.url + ",params:" + JSON.stringify(req.body) + ",error:" + str);
-            } else {
-                logger.error("method:" + req.method + ',url:' + req.url + ",params:" + JSON.stringify(req.query || req.params) + ",error:" + str);
-            }
-            res.json({
-                code: 500,
-                data:err
-            })
-        } else {
-            //开发环境
-            errorNotification(str, req);
-        }
-    } else {
-        next();
-    }
-}
-
-function errorNotification(str, req) {
-    const title = req.method + ':' + req.url;
-    console.info(colors.red('Error info is, "%s" .'), title);
-    console.info(colors.green('Error stack is, "%s" .'), str);
-    require('node-notifier').notify({
-        title: title,
-        message: str
-    })
-}
