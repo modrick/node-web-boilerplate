1da5c8ec379767ac6d774bee6b438052484398a4
name <name@names-Mac-mini.local>
2016-04-01 16:34:19 +0800

    修改为generator的异常处理

--- index.js
+++ index.js
@@ -11,10 +11,10 @@ var app = express();
 // var weixinService = require('./server/service/weixinService');
 var constants = require('./server/helpers/constants');
 var authority = require('./server/filter/authority');
-var domain=require('domain');
-var exception=require('./server/helpers/exception')
-//静态资源拦截器，指定静态资源目录
-app.use(express.static(__dirname + '/public')); 
+var domain = require('domain');
+var exception = require('./server/helpers/exception')
+	//静态资源拦截器，指定静态资源目录
+app.use(express.static(__dirname + '/public'));
 // App 全局配置
 app.use(cookieParser());
 //权限拦截
@@ -22,23 +22,23 @@ app.use(cookieParser());
 //设置跨域
 // app.use(authority.crossDomain);
 app.use(bodyParser.json({
-    limit: '2mb'
+	limit: '2mb'
 }));
 app.use(bodyParser.urlencoded({
-    limit: '2mb',
-    extended: true
+	limit: '2mb',
+	extended: true
 }));
-//捕获异步引起的异常
-app.use(function(req, res, next) {
-     exception.nioExceptionHandle(domain,req,res,next);
+app.addErrorHandle(function(err, req, res) {
+	res.json({
+		code: 500,
+		data: err.toString()
+	});
 });
 //微信开发需要开启
 // weixinService.getAccess_token(); //开启微信获取token定时任务
 // app.use('/weixin', wechat(constants.WeixinConstants.TOKEN, weixinService.payAttentionTo));
 //注入URL
 require('./server/controllers/routes')(app);
-//捕获同步引起的异常
-app.use(exception.bioExceptionHandle);
 
 var appServer = new Server(app);
 appServer.start();
--- server/helpers/commonUtil.js
+++ server/helpers/commonUtil.js
@@ -9,193 +9,14 @@ var Q = require('q');
 
 var utils = {
 
-    getPastDay: function (createDate) {
-        if (!createDate) {
-            return "";
-        }
-        var s = createDate.getTime();
-        var ns = new Date().getTime();
-        var between = ns - s;
-        if (between < 3600000) {
-            var nowString = parseInt(between / (60 * 1000));
-            if (nowString == 0) {
-                return "现在";
-            } else {
-                return parseInt(between / (60 * 1000)) + "分钟以前";
-            }
-        } else if (3600000 <= between && between < 86400000) {
-            // 1小时 ~ 24 小时
-            return parseInt(between / 3600000) + "小时以前";
-        } else if (86400000 <= between && between < 15811200000) {
-            // 一天 ~ 183天(半年)
-            return formatDate(createDate, "M-d h:m");
-        } else {
-            // 大于半年
-            return formatDate(createDate, "yyyy-M-d");
-        }
-    },
-
-    getFutureDay: function (end) {
-        if (!end) {
-            return "";
-        }
-        var today = new Date();
-        var s = today.getTime();
-        var ns = end.getTime();
-        var between = ns - s;
-        if (between < 3600000) {
-            var nowString = parseInt(between / (60 * 1000));
-            if (nowString <= 0) {
-                return "已经结束";
-            } else {
-                return "还剩" + parseInt(between / (60 * 1000)) + "分钟";
-            }
-        } else if ((3600000 <= between) && (between < 86400000)) {
-            return "还剩" + parseInt(between / 3600000) + "小时";
-        } else {
-            return "还剩" + parseInt(between / 86400000) + "天";
-        }
-
-    },
-  
-    /**
-     * @param flag
-     * if flag equals 'f', the text "已经结束" is returned
-     */
-    getFormattedDate: function (date, flag) {
-        if (!date) {
-            return '';
-        } else if (date.constructor == String) {
-            date = new Date(date);
-        }
-        var now = new Date();
-        try {
-            if (now.getTime() < date.getTime()) {
-                return getFutureDay(date);
-            } else if (!!flag && flag.toLowerCase() == 'f') {
-                return '已经结束';
-            } else {
-                return getPastDay(date);
-            }
-        } catch (ex) {
-            console.error(ex);
-            return date;
-        }
-    },
-
-
-    extractHtmlImg: function (str) {
-        var rtn = [];
-        if (str) {
-            return rtn;
-        }
-        var regex = /<img[^<]+[(\/>)$]/g;
-        var _imgArr = str.match(regex);
-        if (!_imgArr || _imgArr.length < 1) {
-            return rtn;
-        }
-        var _srcArr = [], i = 0, _len = _imgArr.length, _elem, _subArr, _src;
-        var regex2 = /src="[^"]+"/i
-        for (; i < _len; i++) {
-            _elem = _imgArr[i];
-            _subArr = _elem.match(regex2);
-            if (!_subArr || _subArr.length < 1) {
-                continue;
-            }
-            _src = _subArr[0].substring(5, _subArr[0].length - 1); // 'src="'.length == 5
-            rtn.push({imgSrc: _src, imgAlt: ''});
-        }
-        return rtn;
-    },
-
-    convertContentType: function (suffix) {
-        if (hdlUtil.trimStr(suffix) == '') {
-            suffix = '';
-        }
-        suffix = suffix.toLowerCase();
-        var contentType = '';
-        switch (suffix) {
-            case 'png':
-                contentType = 'image/png';
-                break;
-            case 'bmp':
-                contentType = 'image/bmp';
-                break;
-            case 'cod':
-                contentType = 'image/cis-cod';
-                break;
-            case 'gif':
-                contentType = 'image/gif';
-                break;
-            case 'ief':
-                contentType = 'image/ief';
-                break;
-            case 'jpe':
-                contentType = 'image/jpeg';
-                break;
-            case 'jpeg':
-                contentType = 'image/jpeg';
-                break;
-            case 'jpg':
-                contentType = 'image/jpeg';
-                break;
-            case 'jfif':
-                contentType = 'image/pipeg';
-                break;
-            case 'svg':
-                contentType = 'image/svg+xml';
-                break;
-            case 'tif':
-                contentType = 'image/tiff';
-                break;
-            case 'tiff':
-                contentType = 'image/tiff';
-                break;
-            case 'ras':
-                contentType = 'image/x-cmu-raster';
-                break;
-            case 'cmx':
-                contentType = 'image/x-cmx';
-                break;
-            case 'ico':
-                contentType = 'image/x-icon';
-                break;
-            case 'pnm':
-                contentType = 'image/x-portable-anymap';
-                break;
-            case 'pbm':
-                contentType = 'image/x-portable-bitmap';
-                break;
-            case 'pgm':
-                contentType = 'image/x-portable-graymap';
-                break;
-            case 'ppm':
-                contentType = 'image/x-portable-pixmap';
-                break;
-            case 'rgb':
-                contentType = 'image/x-rgb';
-                break;
-            case 'xbm':
-                contentType = 'image/x-xbitmap';
-                break;
-            case 'xpm':
-                contentType = 'image/x-xpixmap';
-                break;
-            case 'xwd':
-                contentType = 'image/x-xwindowdump';
-                break;
-            default:
-                contentType = 'text/plain';
-        }
-        return contentType;
-    },
     
+
     //JS中继承的实现--start
-    _isObject: function (o) {
+    _isObject: function(o) {
         return Object.prototype.toString.call(o) === '[object Object]';
     },
 
-    _extend: function (destination, source) {
+    _extend: function(destination, source) {
         var property;
         for (property in destination) {
             if (destination.hasOwnProperty(property)) {
@@ -203,7 +24,6 @@ var utils = {
                 if (this._isObject(destination[property]) && this._isObject(source[property])) {
                     self(destination[property], source[property]);
                 }
-                ;
                 // 若sourc[property]已存在，则跳过
                 if (source.hasOwnProperty(property)) {
                     continue;
@@ -215,23 +35,21 @@ var utils = {
     },
 
     //继承调用方法
-    extend: function () {
-        var arr = arguments,
-            result = {},
-            i;
-        if (!arr.length) return {};
-        for (i = arr.length - 1; i >= 0; i--) {
-            if (this._isObject(arr[i])) {
-                this._extend(arr[i], result);
+    extend: function() {
+            var arr = arguments,
+                result = {},
+                i;
+            if (!arr.length) return {};
+            for (i = arr.length - 1; i >= 0; i--) {
+                if (this._isObject(arr[i])) {
+                    this._extend(arr[i], result);
+                };
             }
-            ;
+            arr[0] = result;
+            return result;
         }
-        arr[0] = result;
-        return result;
-    }
-    //JS中继承的实现--end
+        //JS中继承的实现--end
 
 }
 
 module.exports = utils;
-
