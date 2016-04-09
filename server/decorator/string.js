c19626ad26f92424b2571cb293982615aec82c9f
Slimeria <Slimeria37@gmail.com>
2016-04-10 00:04:58 +0800

    实现属性的字符串类型限制

--- server/decorator/string.js
+++ server/decorator/string.js
@@ -1,10 +1,40 @@
-/*
- * 限制属性为String类型
+/**
+ * 限制属性类型为字符串
  *
  * @param {} 
  * @return {}
  */
-module.exports = function(target, key, descriptor) {
-
-	return descriptor;
+function string(target, key, descriptor) {
+	const initializer = descriptor.initializer
+	if (initializer) {
+		checkString(initializer.call(this))
+	}
+	return {
+		descriptor.configurable,
+			descriptor.enumerable,
+			get() {
+				let value;
+				//class创建的时候属性的初始化赋值，及在没有set值之前,其属性取值由initializer获取，
+				//后面对属性调用过set后，其取值从descriptor.value去获取
+				if (initializer && !descriptor.value) {
+					value = initializer.call(this)
+				} else if (descriptor.value) {
+					value = descriptor.value
+				}
+				return value
+			},
+			set(value) {
+				checkString(value)
+				descriptor.value = key
+			}
+	};
 }
+
+function checkString(value) {
+	if (typeof(value) != 'string') {
+		throw new SyntaxError('@string 属性 ${key} 的值必须是字符串类型。 ');
+	}
+}
+
+
+module.exports = string
