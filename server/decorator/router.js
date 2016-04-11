4e5ee35152f54998eed7248e1b1e8d7fd0d73318
name <name@names-Mac-mini.local>
2016-04-11 14:20:44 +0800

    rouer的decorator 实现5个常用方法

--- server/decorator/router.js
+++ server/decorator/router.js
@@ -10,29 +10,67 @@ function Router(url) {
     return function(classObject) {
         classObject.prototype.initRestFull = function(app) {
             if (!url) url = ''
+            let self = this
             let className = classObject.name.toLowerCase()
-                //获取指定对象
+
+            //获取指定对象
             app.get(url + '/classes/' + className + '/:id', function*(req, res) {
+                let id = req.params.id
+                let data = yield * self.findOne(id)
+                returnJson(res, data)
+            })
 
-                })
-                //获取对象
+            //获取对象
             app.get(url + '/classes/' + className, function*(req, res) {
+                let selector;
+                if (req.query.selector) {
+                    selector = JSON.parse(req.query.selector)
+                } else {
+                    selector = {}
+                }
+                let sort
+                if (req.query.sort) {
+                    sort = JSON.parse(req.query.sort)
+                }
+                let start = req.query.start
+                let limit = req.query.limit
+                if (!limit) {
+                    limit = 0
+                }
+                let data = yield * self.findPagingData(selector, start, limit, sort)
+                returnJson(res, data)
+            })
 
-                })
-                //增加对象
+            //增加对象
             app.post(url + '/classes/' + className, function*(req, res) {
+                let object = req.body
+                let data = yield * self.save(object)
+                returnJson(res, data)
+            })
 
-                })
-                //更新指定对象
+            //更新指定对象
             app.put(url + '/classes/' + className + '/:id', function*(req, res) {
+                let id = req.params.id
+                let object = req.body
+                let data = yield * self.update(id, object)
+                returnJson(res, data)
+            })
 
-                })
-                //删除指定对象
+            //删除指定对象
             app.delete(url + '/classes/' + className + '/:id', function*(req, res) {
-
+                let id = req.params.id
+                let data = yield * self.remove(id)
+                returnJson(res, data)
             })
         }
     }
+}
+
+function returnJson(res, data) {
+    res.json({
+        code: 100,
+        data: data
+    });
 }
 
 module.exports = Router
